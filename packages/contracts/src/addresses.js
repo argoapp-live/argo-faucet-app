804fbba8bbb7ad58b638158e36a23ac086f995c4
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2021-05-27 15:59:05 +0530

    added all the functions for contract interaction

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,7 +1,8 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
+    erc20: "0x02f95e68f345dfbfc69e1ed662bafacb8749e5ab",
+    faucet: "0xAE93C705b74277E3ceE3f6092185E4Cf1fDbc849"
 };
 
 export default addresses;
--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -10,16 +10,7 @@ import useWeb3Modal from "./hooks/useWeb3Modal";
 import { addresses, abis } from "@project/contracts";
 import GET_TRANSFERS from "./graphql/subgraph";
 
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = getDefaultProvider();
-  // Create an instance of an ethers.js Contract
-  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
-  // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
-  console.log({ tokenBalance: tokenBalance.toString() });
-}
+
 
 function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
   return (
@@ -40,6 +31,46 @@ function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
 function App() {
   const { loading, error, data } = useQuery(GET_TRANSFERS);
   const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+
+  async function getFaucetContract(web3) {
+    const contract = new web3.eth.Contract(abis.faucet, addresses.faucet);
+    return contract;
+  }
+  async function getErc20Contract(web3) {
+    const contract = new web3.eth.Contract(abis.erc20, addresses.erc20);
+    return contract;
+  }
+
+  async function faucetBalance() {
+    const contract = await getErc20Contract(provider);
+    const wallet = await provider.eth.getAccounts();
+    var balance = await contract.methods
+      .balanceOf(addresses.faucet)
+      .call();
+    return provider.utils.fromWei(balance);
+
+  }
+  async function userBalance() {
+    const contract = await getErc20Contract(provider);
+    const wallet = await provider.eth.getAccounts();
+    var balance = await contract.methods
+      .balanceOf(wallet[0])
+      .call();
+    return provider.utils.fromWei(balance);
+
+  }
+  async function getToken() {
+    const contract = await getFaucetContract(provider);
+    const wallet = await provider.eth.getAccounts();
+    var tx = await contract.methods.withdraw(addresses.erc20).send({ from: wallet[0] });
+    return tx
+
+  }
+
+  async function getUserAddress() {
+    const wallet = await provider.eth.getAccounts();
+    return wallet[0];
+  }
 
   React.useEffect(() => {
     if (!loading && !error && data && data.transfers) {
