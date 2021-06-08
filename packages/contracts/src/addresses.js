cd636c50ea5c297f770953dcc1ad172db9b7bb78
Nitin Sharma <36033958+nitinshr135@users.noreply.github.com>
2021-06-08 11:56:38 +0530

    Update addresses.js

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,8 +1,8 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-    erc20: "0xaFd47fD727811804E975b790c69f756538b39F40",
-    faucet: "0x91539B4B5D72B6940fE5FED58CEA2A06AD6A2FCe"
+    erc20: "0x135d0CabDF539dc82121a48b5936ee3E3F785558",
+    faucet: "0xb8C0feDfc362BBc176ac7C037cE0E723c57f516A"
 };
 
 export default addresses;
