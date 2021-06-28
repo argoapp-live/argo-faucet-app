3a7188ea8cb155a1cf917e3214156996937ffb65
nitinshr135 <nitinshr135@gmail.com>
2021-06-28 18:38:50 +0530

    feat: updated erc20 and faucet address

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -1,8 +1,8 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-    erc20: "0x135d0CabDF539dc82121a48b5936ee3E3F785558",
-    faucet: "0xb8C0feDfc362BBc176ac7C037cE0E723c57f516A"
+    erc20: "0xE044842Ce0A54dF5Dc11dbB962B462B28331728e",
+    faucet: "0x52bE3144b26326836Cb25f05795679d98F3caF0B"
 };
 
 export default addresses;
